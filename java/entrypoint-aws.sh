b74e51de029f8461ed78845d813ef0dc7d45ae62
Joost van der Gaag <joost@smartfarmsensing.com>
2022-06-16 15:36:16 +0200

    Added changes to extract data volumes out of the containers. configurations and data volumes are created for docker and can be used in AWS

---
+++ java/entrypoint-aws.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while sleep 10;
+    do 
+        api_key=`cat /openmlconfig/api_key.txt`; 
+        echo "API key:$api_key";  
+        java -Xmx4G -jar /openmlsource/Java/evaluate.jar -config "server = http://localhost/;api_key = $api_key" -f process_dataset;
+    done
